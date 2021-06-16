334fcca414039e338d4e8aea2ab19b192d6b9bda
Particular <alexandresimoesf@gmail.com>
2021-06-16 11:27:18 -0300

    tuple added

--- anamnese_sql.py
+++ anamnese_sql.py
@@ -4,13 +4,17 @@ from itertools import starmap
 
 def anamnese(*args):
     with open('HISTORIC_NOVO.csv', 'r', newline='\n', encoding='latin-1') as file:
-        reader = iter(csv.DictReader(file, delimiter=';'))
-        linha = next(reader)
-        codigo , data = linha['CodPaciente'], '{}{}{}{}/{}{}/{}{}'.format(*linha['DataConsulta'])
-
-        for id, data_agenda in args[0]:
-            print(id, data_agenda)
-            print(codigo, data)
-            print('#' * 10)
+        reader = csv.DictReader(file, delimiter=';')
+        # reader = list(reader.items())
+        # linha = next(reader)
+        for i in reader:
+            print(list(i.items()))
+            break
+        # codigo , data = linha['CodPaciente'], '{}{}{}{}/{}{}/{}{}'.format(*linha['DataConsulta'])
+        #
+        # for id, data_agenda in args[0]:
+        #     print(id, data_agenda)
+        #     print(codigo, data)
+        #     print('#' * 10)
         file.close()
         exit()

6979a24f668b29b9563efad86cdb5dd0062e10cf
Particular <alexandresimoesf@gmail.com>
2021-06-16 15:25:24 -0300

    concatenar

--- anamnese_sql.py
+++ anamnese_sql.py
@@ -1,21 +1,16 @@
 import csv
-from collections import defaultdict
 
 
 def anamnese(*args):
     with open('HISTORIC_NOVO.csv', 'r', newline='\n', encoding='latin-1') as file:
         reader = csv.DictReader(file, delimiter=';')
-        for i in reader:
-            if i['CodPaciente'] == str(args[0][0][0]):
-                print(i)
-                break
-        # linha = next(reader)
-
-        # codigo , data = linha['CodPaciente'], '{}{}{}{}/{}{}/{}{}'.format(*linha['DataConsulta'])
-        #
-        # for id, data_agenda in args[0]:
-        #     print(id, data_agenda)
-        #     print(codigo, data)
-        #     print('#' * 10)
+        for codigo_agenda, data_agenda in args[0]:
+            print('#' * 10)
+            print(codigo_agenda, data_agenda)
+            for linha in reader:
+                codigo, data = linha['CodPaciente'], '{}{}{}{}/{}{}/{}{}'.format(*linha['DataConsulta'])
+                if codigo == codigo_agenda and data == data_agenda:
+                    if linha['Historico'] != 'null':
+                        print(linha['Historico'])
         file.close()
         exit()

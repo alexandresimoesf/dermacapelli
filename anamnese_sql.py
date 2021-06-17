0fa0917c8dde3ca9621f862ff363bf3cc092c2b9
Particular <alexandresimoesf@gmail.com>
2021-06-17 09:18:55 -0300

    teste

--- anamnese_sql.py
+++ anamnese_sql.py
@@ -1,27 +1,35 @@
 import csv
+from collections import deque
+from itertools import islice
 
 medicos: dict = {'20': '203',
                  '25': '210',
                  '26': '212',
                  '2': '211'}
 
+d = deque(maxlen=2)
+
 
 def anamnese(*args):
     with open('HISTORIC_NOVO.csv', 'r', newline='\n', encoding='latin-1') as file:
+        inicio = True
         reader = csv.DictReader(file, delimiter=';')
         sql_inicio = 'INSERT INTO public.anamnese('
         colunas = 'anamnese, checksum, datacriacao, fk_responsavel_id, fk_prontuario_id) VALUES ('
-
         with open('anamnese_sql.sql', 'w', encoding='utf-8') as sql:
             for codigo_agenda, data_agenda in args[0]:
                 historic = ''
                 # print('#' * 10)
                 # print(codigo_agenda, data_agenda)
-                for linha in reader:
+                for index, linha in enumerate(reader):
                     if linha['CodMedico'] in medicos.keys():
                         codigo, data = linha['CodPaciente'], '{}{}{}{}/{}{}/{}{}'.format(*linha['DataConsulta'])
                         if codigo == codigo_agenda and data == data_agenda:
                             if linha['Historico'] != 'null':
+                                if not linha['CodPaciente'] in d and len(d) == 2:
+                                    print('Novo paciente')
+                                print(index, linha['CodPaciente'])
+                                d.append(linha['CodPaciente'])
                                 historic += linha['Historico'] + '. '
                             historic = historic.replace('<Crlf>', '')
                             historic = historic.replace('<CRLF>', '')

e84bc2abde8c075d0069839574885b2d38bf49b2
Particular <alexandresimoesf@gmail.com>
2021-06-18 12:09:09 -0300

    insert_no_banco codigo

--- insert_no_banco.py
+++ insert_no_banco.py
@@ -2,6 +2,13 @@ import psycopg2
 
 con = psycopg2.connect(host='localhost', database='doctor247', user='postgres', password='123456789')
 cur = con.cursor()
-sql = ""
-cur.execute(sql)
-con.commit()
+with open('agenda_sql_203.sql', 'r', encoding='utf-8') as sql_file:
+    for linha_sql in sql_file:
+        try:
+            sql = linha_sql
+            cur.execute(sql)
+        except Exception as e:
+            print(e)
+
+# con.commit()
+con.close()

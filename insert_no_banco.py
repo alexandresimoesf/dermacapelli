189b3b21177cc7e17e146adc6731950e7182a55a
Particular <alexandresimoesf@gmail.com>
2021-06-18 12:17:31 -0300

    .rollback()

--- insert_no_banco.py
+++ insert_no_banco.py
@@ -3,12 +3,13 @@ import psycopg2
 con = psycopg2.connect(host='localhost', database='doctor247', user='postgres', password='123456789')
 cur = con.cursor()
 with open('agenda_sql_203.sql', 'r', encoding='utf-8') as sql_file:
-    for linha_sql in sql_file:
+    for n, linha_sql in enumerate(sql_file):
         try:
             sql = linha_sql
             cur.execute(sql)
         except Exception as e:
-            print(e)
+            con.rollback()
+            print(n, e)
 
 # con.commit()
 con.close()
